93f37eb41b8c7c2ef680cc1f9b0f8a0ef45a20a3
paulolageN <paullolageneto@proton.me>
2026-06-15 20:14:39 -0300

    updates

--- app/src/main/java/com/example/myapplication/CadastroDeRestauranteActivity.java
+++ app/src/main/java/com/example/myapplication/CadastroDeRestauranteActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CadastroDeRestauranteActivity extends AppCompatActivity {
+import com.example.myapplication.databinding.ActivityCadastroDeRestauranteBinding;
 
+import controller.RestauranteController;
+import model.Restaurante;
+import model.Usuario;
+
+public class CadastroDeRestauranteActivity extends AppCompatActivity {
+    ActivityCadastroDeRestauranteBinding binding;
+    RestauranteController restauranteController;
+    Restaurante restaurante;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,30 @@ public class CadastroDeRestauranteActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        restauranteController = new RestauranteController(CadastroDeRestauranteActivity.this);
+
+        binding.btnCadastrarRestaurante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restaurante = new Restaurante();
+                restaurante.setNomeRestaurante(binding.editNomeRestaurante.getText().toString());
+                restaurante.setEnderecoRestaurante(binding.editEnderecoRestaurante.getText().toString());
+                restaurante.setTelefoneRestaurante(binding.editTelefoneRestaurante.getText().toString());
+                restaurante.setDescricao(binding.editDescricao.getText().toString());
+                restaurante.setHorarioFuncionamento(binding.editHorarioRestaurante.getText().toString());
+
+                if(restauranteController.inserir(restaurante)){
+                    Toast.makeText(CadastroDeRestauranteActivity.this,"Seu cadastro foi realizado com sucesso!", Toast.LENGTH_LONG).show();
+                    //Chamar a proxima tela principal
+                    Intent intent = new Intent(CadastroDeRestauranteActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(CadastroDeRestauranteActivity.this,"Erro ao realizar o seu cadastro!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
